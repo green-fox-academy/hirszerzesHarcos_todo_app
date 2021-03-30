c95a2bf919c8da55028b7a32712a96efe3b9f9a0
hirszerzesHarcos <hirszerzesharcos@gmail.com>
2021-03-30 06:10:01 +0200

    todo app was created

---
+++ src/ToDoMain.java
@@ -0,0 +1,25 @@
+public class ToDoMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello To Do App!!!");
+    System.out.println("Tessaiga!!!");
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
+    }
+//    Path path = Paths.get("");
+//    System.out.println(path.toAbsolutePath());
+//    System.out.println(path.toAbsolutePath().getParent());
+    if (args.length == 0) {
+      String lista = "Parancssori Todo applikáció\n" +
+              "=============================\n" +
+              "\n" +
+              "Parancssori argumentumok:\n" +
+              "    -l   Kilistázza a feladatokat\n" +
+              "    -a   Új feladatot ad hozzá\n" +
+              "    -r   Eltávolít egy feladatot\n" +
+              "    -c   Teljesít egy feladatot";
+      System.out.println(lista);
+      Integer var = 4;
+    }
+  }
+}
